8b6c6123e906aa60cc6bbcf328d78383bd944793
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-09-30 19:30:30 +0900

    ✨ 書籍の所持状況確認ページの実装

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["thumbnail.image.rakuten.co.jp", "cover.openbd.jp"],
+  },
+};
